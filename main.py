607b9f38299d980aebdffb5c1e6ae9d9c93ac44d
Inbar eliyahu <inbarel@post.bgu.ac.il>
2021-12-11 20:31:32 +0200

    update

---
+++ main.py
@@ -0,0 +1,16 @@
+import csv
+import sqlite3
+
+
+def main():
+    con = sqlite3.connect("adults.db")
+    cur = con.cursor()
+    file = open("adult.csv")
+    rows = csv.reader(file)
+    cur.execute("CREATE TABLE adult (age,relationship,race,gender);")
+    cur.executemany("INSERT INTO adult VALUES (?, ?,?,?)", rows)
+    cur.execute("SELECT * FROM adult")
+    print(cur.fetchall())
+
+    print('please enter your k: ')
+    k = input()
